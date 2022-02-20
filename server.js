ce35f583d49082ba0a67c7999fba84a197ee641b
uk960214 <uk960214@gmail.com>
2022-02-20 22:39:56 +0900

    add db

--- server.js
+++ server.js
@@ -4,7 +4,32 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const knex = require("knex");
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "fis960214",
+    database: "choice",
+  },
+});
+
+console.log(
+  db
+    .select("*")
+    .from("Choice")
+    .then((data) => {
+      console.log(data);
+    })
+);
+
 app.get("/", (req, res) => {
+  db("Choice").insert({
+    userId: req.query.userId,
+    sevenTrust: req.query.sevenTrust,
+    nineTrust: req.query.nineTrust,
+  });
   const resData = {
     type: "RealWorld.Commands.DisplayHtml",
     parameters: {
