b30791bb73f14d6dce1082e0dc9aad6a6bccc724
uk960214 <uk960214@gmail.com>
2022-02-20 23:30:36 +0900

    update database

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-tetrahedral-22898",
     user: "postgres",
     password: "fis960214",
     database: "choice",
