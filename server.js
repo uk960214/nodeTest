07c6bc16e6b4c943788a0dcad6ede47b972b2949
uk960214 <uk960214@gmail.com>
2022-02-20 21:46:42 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const resData = {
+    type: "RealWorld.Commands.DisplayHtml",
+    parameters: {
+      content: `<p>${req.query.userId}</p>`,
+    },
+  };
+  res.send(resData);
+});
+
+app.listen(3000);
