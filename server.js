a1aec3ccd221487608ee7b183b3fd93112c8d56e
uk960214 <uk960214@gmail.com>
2022-03-03 00:24:39 +0900

    edit for invalid request

--- server.js
+++ server.js
@@ -14,35 +14,38 @@ const db = knex({
 });
 
 app.get("/", (req, res) => {
-  const sevenTrust = req.query.sevenTrust === "true";
-  const nineTrust = req.query.nineTrust === "true";
+  if (req.query.userId) {
+    const sevenTrust = req.query.sevenTrust === "true";
+    const nineTrust = req.query.nineTrust === "true";
 
-  db("choice")
-    .insert({
-      userid: req.query.userId,
-      seventrust: sevenTrust,
-      ninetrust: nineTrust,
-    })
-    .then(() => {
-      db("choice")
-        .where({ seventrust: sevenTrust, ninetrust: nineTrust })
-        .count("userid")
-        .then((data) => data[0].count)
-        .then((count) => {
-          db("choice")
-            .count("id")
-            .then((data) => {
-              const percentage = Math.floor((count / data[0].count) * 100);
-              const resData = {
-                type: "RealWorld.Commands.DisplayHtml",
-                parameters: {
-                  content: `<p>${percentage}%의 사용자가 선택했습니다.</p>`,
-                },
-              };
-              res.send(resData);
-            });
-        });
-    });
+    db("choice")
+      .insert({
+        userid: req.query.userId,
+        seventrust: sevenTrust,
+        ninetrust: nineTrust,
+      })
+      .then(() => {
+        db("choice")
+          .where({ seventrust: sevenTrust, ninetrust: nineTrust })
+          .count("userid")
+          .then((data) => data[0].count)
+          .then((count) => {
+            db("choice")
+              .count("id")
+              .then((data) => {
+                const percentage = Math.floor((count / data[0].count) * 100);
+                const resData = {
+                  type: "RealWorld.Commands.DisplayHtml",
+                  parameters: {
+                    content: `<p>${percentage}%의 사용자가 선택했습니다.</p>`,
+                  },
+                };
+                res.send(resData);
+              });
+          });
+      });
+  }
+  res.send("invalid request");
 });
 
 app.get("/read-only", (req, res) => {
