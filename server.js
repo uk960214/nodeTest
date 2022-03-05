a359af359b5b9b71d1420c104ae9852801b772df
uk960214 <uk960214@gmail.com>
2022-03-05 13:22:28 +0900

    update response style

--- server.js
+++ server.js
@@ -38,14 +38,16 @@ app.get("/", (req, res) => {
                   type: "RealWorld.Commands.DisplayHtml",
                   parameters: {
                     content: `
-                    <h1>엔딩 선택 통계</h1>
+                    <h1 style="text-align:center;">엔딩 선택 통계</h1>
+                    <br />
+                    <br />
+                    <br />
                     <p>세븐은 '${
-                      sevenTrust ? "신뢰" : "불신"
-                    }을 선택했고, 나인은 '${
-                      nineTrust ? "신뢰" : "불신"
-                    }'을 선택했습니다.<p>
-                    <p>${percentage}%의 사용자가 위와 같은 선택을 내렸습니다.</p>
-                    `,
+                      sevenTrust ? "신뢰를" : "불신을"
+                    }' 선택했고, 나인은 '${
+                      nineTrust ? "신뢰를" : "불신을"
+                    }' 선택했습니다.<p>
+                    <p><strong style="color: red; font-size: 1.05rem;">${percentage}%</strong>의 사용자가 위와 같은 선택을 내렸습니다.</p>`,
                   },
                 };
                 res.send(resData);
@@ -71,11 +73,16 @@ app.get("/read-only", (req, res) => {
             type: "RealWorld.Commands.DisplayHtml",
             parameters: {
               content: `
-              <h1>엔딩 선택 통계</h1>
-              <p>세븐은 '${sevenTrust ? "신뢰" : "불신"}을 선택했고, 나인은 '${
-                nineTrust ? "신뢰" : "불신"
-              }'을 선택했습니다.<p>
-                    <p>${percentage}%의 사용자가 위와 같은 선택을 내렸습니다.</p>`,
+              <h1 style="text-align:center;">엔딩 선택 통계</h1>
+              <br />
+              <br />
+              <br />
+              <p>세븐은 '${
+                sevenTrust ? "신뢰를" : "불신을"
+              }' 선택했고, 나인은 '${
+                nineTrust ? "신뢰를" : "불신을"
+              }' 선택했습니다.<p>
+              <p><strong style="color: red; font-size: 1.05rem;">${percentage}%</strong>의 사용자가 위와 같은 선택을 내렸습니다.</p>`,
             },
           };
           res.send(resData);
