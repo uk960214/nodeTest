8777749989fa9af4220f5dbdb8f8f97b7d739567
uk960214 <uk960214@gmail.com>
2022-03-05 13:25:46 +0900

    update response style

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
                   type: "RealWorld.Commands.DisplayHtml",
                   parameters: {
                     content: `
-                    <h1 style="text-align:center;">엔딩 선택 통계</h1>
+                    <h2 style="text-align:center;">엔딩 선택 통계</h2>
                     <br />
                     <br />
                     <br />
@@ -47,7 +47,9 @@ app.get("/", (req, res) => {
                     }' 선택했고, <br /> 나인은 '${
                       nineTrust ? "신뢰를" : "불신을"
                     }' 선택했습니다.<p>
-                    <p><strong style="color: red; font-size: 1.05rem;">${percentage}%</strong>의 사용자가 위와 같은 선택을 내렸습니다.</p>`,
+                    <br />
+                    <br />
+                    <p><strong style="color: red; font-size: 1.1rem;">${percentage}%</strong>의 사용자가 위와 같은 선택을 내렸습니다.</p>`,
                   },
                 };
                 res.send(resData);
@@ -73,7 +75,7 @@ app.get("/read-only", (req, res) => {
             type: "RealWorld.Commands.DisplayHtml",
             parameters: {
               content: `
-              <h1 style="text-align:center;">엔딩 선택 통계</h1>
+              <h2 style="text-align:center;">엔딩 선택 통계</h2>
               <br />
               <br />
               <br />
@@ -82,7 +84,9 @@ app.get("/read-only", (req, res) => {
               }' 선택했고, <br /> 나인은 '${
                 nineTrust ? "신뢰를" : "불신을"
               }' 선택했습니다.<p>
-              <p><strong style="color: red; font-size: 1.05rem;">${percentage}%</strong>의 사용자가 위와 같은 선택을 내렸습니다.</p>`,
+              <br />
+              <br />
+              <p><strong style="color: red; font-size: 1.1rem;">${percentage}%</strong>의 사용자가 위와 같은 선택을 내렸습니다.</p>`,
             },
           };
           res.send(resData);
