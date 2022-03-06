5dde07f07e9e0eafe07f2261b211c6b6ab0aff44
uk960214 <uk960214@gmail.com>
2022-03-06 15:16:26 +0900

    create result variable

--- server.js
+++ server.js
@@ -13,6 +13,66 @@ const db = knex({
   },
 });
 
+const resultTemplate = ({ sevenTrust, nineTrust, percentage }) => {
+  return `
+    <style>
+      @import url('https://fonts.googleapis.com/css?family=Nanum+Myeongjo&display=swap');
+
+      * {
+        font-family: 'Nanum Myeongjo', serif;
+      }
+
+      .container {
+        text-align: center;
+      }
+
+      .agent-name,
+      .choice,
+      .result-percent {
+        font-size: 1.1rem;
+      }
+
+      .trust {
+        color: green;
+      }
+
+      .distrust {
+        color: red;
+      }
+      
+      .result-percent {
+        color: yellow
+      }
+    </style>
+    <div class="container">
+      <h3>엔딩 선택 통계</h3>
+      <br />
+      <br />
+      <p class="choice-explain">
+        <strong class="agent-name">세븐</strong>은 ${
+          sevenTrust
+            ? "<strong class='choice trust'>[신뢰]</strong>를"
+            : "<strong class='choice distrust'>[불신]</strong>을"
+        }
+        <br />
+        <strong class="agent-name">나인</strong>은 ${
+          nineTrust
+            ? "<strong class='choice trust'>[신뢰]</strong>를"
+            : "<strong class='choice distrust'>[불신]</strong>을"
+        } <br />
+        선택했습니다.
+      </p>
+      <br />
+      <br />
+      <p class="percent-explain">
+        <strong class="result-percent">${percentage}%</strong>의 플레이어가
+        <br />
+        같은 선택을 했습니다.
+      </p>
+    </div>
+  `;
+};
+
 app.get("/", (req, res) => {
   if (req.query.userId) {
     const sevenTrust = req.query.sevenTrust === "true";
@@ -37,67 +97,11 @@ app.get("/", (req, res) => {
                 const resData = {
                   type: "RealWorld.Commands.DisplayHtml",
                   parameters: {
-                    content: `
-                    <style>
-                      @import url('https://fonts.googleapis.com/css?family=Nanum+Myeongjo&display=swap');
-
-                      * {
-                        font-family: 'Nanum Myeongjo', serif;
-                      }
-
-                      p {
-                        font-family: 'Nanum Myeongjo', serif;
-                      }
-
-                      .container {
-                        text-align: center;
-                      }
-
-                      .agent-name,
-                      .choice,
-                      .result-percent {
-                        font-size: 1.1rem;
-                      }
-
-                      .trust {
-                        color: green;
-                      }
-
-                      .distrust {
-                        color: red;
-                      }
-                      
-                      .result-percent {
-                        color: yellow
-                      }
-                    </style>
-                    <div class="container">
-                      <h3>엔딩 선택 통계</h3>
-                      <br />
-                      <br />
-                      <p>
-                        <strong class="agent-name">세븐</strong>은 ${
-                          sevenTrust
-                            ? "<strong class='choice trust'>[신뢰]</strong>를"
-                            : "<strong class='choice distrust'>[불신]</strong>을"
-                        }
-                        <br />
-                        <strong class="agent-name">나인</strong>은 ${
-                          nineTrust
-                            ? "<strong class='choice trust'>[신뢰]</strong>를"
-                            : "<strong class='choice distrust'>[불신]</strong>을"
-                        } <br />
-                        선택했습니다.
-                      </p>
-                      <br />
-                      <br />
-                      <p>
-                        <strong class="result-percent">${percentage}%</strong>의 플레이어가
-                        <br />
-                        같은 선택을 했습니다.
-                      </p>
-                    </div>
-            `,
+                    content: resultTemplate({
+                      sevenTrust,
+                      nineTrust,
+                      percentage,
+                    }),
                   },
                 };
                 res.send(resData);
@@ -122,67 +126,11 @@ app.get("/read-only", (req, res) => {
           const resData = {
             type: "RealWorld.Commands.DisplayHtml",
             parameters: {
-              content: `
-              <style>
-                @import url('https://fonts.googleapis.com/css?family=Nanum+Myeongjo&display=swap');
-
-                * {
-                  font-family: 'Nanum Myeongjo', serif;
-                }
-
-                p {
-                  font-family: 'Nanum Myeongjo', serif;
-                }
-
-                .container {
-                  text-align: center;
-                }
-
-                .agent-name,
-                .choice,
-                .result-percent {
-                  font-size: 1.1rem;
-                }
-
-                .trust {
-                  color: green;
-                }
-
-                .distrust {
-                  color: red;
-                }
-                
-                .result-percent {
-                  color: yellow
-                }
-              </style>
-              <div class="container">
-                <h3>엔딩 선택 통계</h3>
-                <br />
-                <br />
-                <p>
-                  <strong class="agent-name">세븐</strong>은 ${
-                    sevenTrust
-                      ? "<strong class='choice trust'>[신뢰]</strong>를"
-                      : "<strong class='choice distrust'>[불신]</strong>을"
-                  }
-                  <br />
-                  <strong class="agent-name">나인</strong>은 ${
-                    nineTrust
-                      ? "<strong class='choice trust'>[신뢰]</strong>를"
-                      : "<strong class='choice distrust'>[불신]</strong>을"
-                  } <br />
-                  선택했습니다.
-                </p>
-                <br />
-                <br />
-                <p>
-                  <strong class="result-percent">${percentage}%</strong>의 플레이어가
-                  <br />
-                  같은 선택을 했습니다.
-                </p>
-              </div>
-            `,
+              content: resultTemplate({
+                sevenTrust,
+                nineTrust,
+                percentage,
+              }),
             },
           };
           res.send(resData);
